6fd5d54fb844ab60756ffd4c78b9dfb3ba5c412d
apancoast <pancoastashley@gmail.com>
2025-02-21 10:52:52 -0500

    Add row count validations. Removed CMS source. Fixed seed names and config settings.

---
+++ dbt_hdpt/macros/row_count_equal_to_source_file.sql
@@ -0,0 +1,31 @@
+{%- test row_count_equal_to_source_file(source_name, table_name, model) -%}
+
+{%- set compiled_source = source(source_name, table_name) | string -%}
+{%- set file_extension = compiled_source.split('.')[-1] | replace("'", "") | lower -%}
+
+{%- if file_extension == 'json' -%}
+    {% set read_function = 'read_json' %}
+    {% set count_expression = 'SELECT count(*) - 1 AS expected_row_count FROM ' + read_function + '(' + compiled_source + ')' %}
+{%- elif file_extension in ['csv', 'txt'] -%}
+    {% set read_function = 'read_csv' %}
+    {% set count_expression = 'SELECT count(*) AS expected_row_count FROM ' + read_function + '(' + compiled_source + ')' %}
+{%- elif file_extension == 'shp' -%}
+    {% set read_function = 'ST_read' %}
+    {% set count_expression = 'SELECT count(*) AS expected_row_count FROM ' + read_function + '(' + compiled_source + ')' %}
+{%- else -%}
+    {% set count_expression = 'SELECT count(*) AS expected_row_count FROM ' + compiled_source %}
+{%- endif -%}
+
+WITH expected_count AS (
+    {{ count_expression }}
+),
+actual_count AS (
+    SELECT COUNT(*) AS actual_row_count
+    FROM {{ model }}
+)
+SELECT 
+    *
+FROM expected_count, actual_count
+WHERE actual_row_count <> expected_row_count
+
+{%- endtest -%}
