569ad1b66c7d0686c96bcf944be06e0d0630a61e
apancoast <98405259+apancoast@users.noreply.github.com>
2025-03-10 17:46:38 -0400

    Changed model due to target mart model design changes.

--- dbt_hdpt/models/marts/int_gtfs_stops_aggregated.sql
+++ dbt_hdpt/models/marts/int_gtfs_stops_aggregated.sql
@@ -1,33 +1,42 @@
-with interest_window as ( -- Weekday and 7am to 7pm defines "window" of interest, referenced by later columns
-    SELECT 
-        st.stop_id
+WITH interest_window AS (-- Week day and 7am to 7pm defines window of interest 
+    SELECT
+        ct.census_tract
+        , t.trip_id
         , st.arrival_time
+        , t.route_id
+        , ct.stop_id
+        , t.trip_id
     FROM stg_gtfs__stop_times st
     JOIN stg_gtfs__trips t ON st.trip_id = t.trip_id
+    join int_gtfs_stops_enriched ct on st.stop_id = ct.stop_id
     WHERE 
         st.pickup_type = 0
-        and t.service_id like '%Weekday%' -- Typical PCP work days
-        and date_part('hour', st.arrival_time) between 7 and 19 -- Typical PCP business hours 
+        and t.service_id like '%Weekday%'   -- Typical PCP work days
+        and date_part('hour', st.arrival_time) between 7 and 19 -- Typical PCP business
 )
-, headway_calculations as (
+, counts as (
     SELECT
-        stop_id
-        , COUNT(arrival_time) AS total_pickups_in_window -- Number of pickups throughout the day
+        census_tract
+        , COUNT(DISTINCT route_id) AS number_of_routes
+        , count(distinct stop_id) as number_of_stops
+        , count(distinct trip_id) as number_of_trips
+    from interest_window
+    group by census_tract
+)
+, trips_per_hour AS (
+    SELECT
+        census_tract,
+        DATE_PART('hour', arrival_time) AS hour_of_day,
+        COUNT(DISTINCT trip_id) AS unique_trips_in_hour
     FROM interest_window
-    group by stop_id
+    GROUP BY census_tract, hour_of_day
 )
-, routes as (
-    select
-        st.stop_id
-        , count(distinct t.route_id) as route_counts -- Number of routes that use stop
-    FROM stg_gtfs__stop_times st
-    JOIN stg_gtfs__trips t ON st.trip_id = t.trip_id
-    GROUP by st.stop_id
-)
-SELECT 
-    calcs.stop_id
-    , route_counts 
-    , total_pickups_in_window
-FROM headway_calculations calcs
-JOIN routes rts on rts.stop_id = calcs.stop_id
-;
+SELECT
+    c.census_tract
+    , c.number_of_routes
+    , c.number_of_stops
+    , c.number_of_trips
+    , MEDIAN(tph.unique_trips_in_hour) AS median_trips_per_hour
+FROM counts c
+JOIN trips_per_hour tph on c.census_tract = tph.census_tract
+GROUP BY ALL
